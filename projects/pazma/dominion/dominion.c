474f5cd91dfd8e8dc587308132e51d4c147322a2
pazma <pazma@oregonstate.edu>
2019-01-27 14:58:15 -0800

    Fixed adventurerCard arg error

--- projects/pazma/dominion/dominion.c
+++ projects/pazma/dominion/dominion.c
@@ -709,7 +709,7 @@ int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState
   switch( card ) 
     {
     case adventurer:
-		return adventurerCard(drawntreasure, state, currentPlayer, cardDrawn, temphand[])
+		return adventurerCard(drawntreasure, state, currentPlayer, cardDrawn, temphand)
 
 			
     case council_room:
