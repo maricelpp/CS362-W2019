395bb7f1bd10a9e47e9b33b4ec823801b103e04c
Xiaoqiong Dong <dongxiao@flip3.engr.oregonstate.edu>
2019-03-11 17:17:58 -0700

    final project

--- projects/dongxiao/FinalProject/URLValidatorCorrect/test/UrlValidatorTest.java
+++ projects/dongxiao/FinalProject/URLValidatorCorrect/test/UrlValidatorTest.java
@@ -14,9 +14,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-//package org.apache.commons.validator.routines;
-
-//import org.apache.commons.validator.ResultPair;
 
 import junit.framework.TestCase;
 
